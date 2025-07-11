878124f042a81bccd24f92246ac9278c03faef3a
Sahil Srivastava <sahil.srivastava1612@gmail.com>
2025-07-11 13:15:46 +0530

    Modified Relative Path of Modules

--- DEV_UAT_PROD/DEV_ENV/main.tf
+++ DEV_UAT_PROD/DEV_ENV/main.tf
@@ -1,56 +1,56 @@
 module "azurerm_resource_group" {     #RG
-  source          = "../DEV_UAT_PROD/Project-A_Infra_modules/azurerm_resource_group"
+  source          = "../Project-A_Infra_modules/azurerm_resource_group"
   resource_groups = var.resource_group
 }
 
 module "azurerm_key_vault" {
   depends_on = [module.azurerm_resource_group]
-  source     = "../DEV_UAT_PROD/Project-A_Infra_modules/azurerm_key_vaults"
+  source     = "../Project-A_Infra_modules/azurerm_key_vaults"
   key_vaults = var.key_vault
 }
 
 module "azurerm_vnet" {
   depends_on = [module.azurerm_resource_group]
-  source     = "../DEV_UAT_PROD/Project-A_Infra_modules/azurerm_virtualnetwork"
+  source     = "../Project-A_Infra_modules/azurerm_virtualnetwork"
   vnets      = var.vnet
 }
 
 module "azurerm_subnet" {
   depends_on = [module.azurerm_vnet]
-  source     = "../DEV_UAT_PROD/Project-A_Infra_modules/azurerm_subnets"
+  source     = "../Project-A_Infra_modules/azurerm_subnets"
   subnets    = var.subnet
 }
 
 module "azurerm_ms_sql_server" {
   depends_on  = [module.azurerm_key_vault, module.azurerm_resource_group]
-  source      = "../DEV_UAT_PROD/Project-A_Infra_modules/azurerm_sql_server"
+  source      = "../Project-A_Infra_modules/azurerm_sql_server"
   sql_servers = var.sql_server
 }
 
 module "azurerm_sql_server_database" {
   depends_on    = [module.azurerm_resource_group, module.azurerm_ms_sql_server]
-  source        = "../DEV_UAT_PROD/Project-A_Infra_modules/azurerm_sql_database"
+  source        = "../Project-A_Infra_modules/azurerm_sql_database"
   sql_databases = var.sql_server_database
 }
 
 module "azurerm_nsg" {
   depends_on = [module.azurerm_subnet, module.azurerm_resource_group]
-  source     = "../DEV_UAT_PROD/Project-A_Infra_modules/azurerm_nsg"
+  source     = "../Project-A_Infra_modules/azurerm_nsg"
 }
 
 module "azurerm_subnet_nsg_asso" {
   depends_on = [module.azurerm_resource_group, module.azurerm_nsg, module.azurerm_subnet, module.azurerm_vnet]
-  source     = "../DEV_UAT_PROD/Project-A_Infra_modules/azurerm_subnsg_association"
+  source     = "../Project-A_Infra_modules/azurerm_subnsg_association"
 }
 
 module "azurerm_public_ip" {
   depends_on = [module.azurerm_resource_group]
-  source     = "../DEV_UAT_PROD/Project-A_Infra_modules/azurerm_public_ips"
+  source     = "../Project-A_Infra_modules/azurerm_public_ips"
   pips       = var.pip
 }
 
 module "azurerm_virtual_machine" {
   depends_on = [module.azurerm_key_vault, module.azurerm_nsg, module.azurerm_public_ip, module.azurerm_resource_group, module.azurerm_subnet, module.azurerm_vnet]
-  source     = "../DEV_UAT_PROD/Project-A_Infra_modules/azurerm_virtual_machines"
+  source     = "../Project-A_Infra_modules/azurerm_virtual_machines"
   vms        = var.vm
 }
